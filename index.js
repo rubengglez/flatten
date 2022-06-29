7423f1b1665966a8dcdab2833ff582d7ede80b14
Rubén <ruben.garcia@wallbox.com>
2022-06-29 08:11:54 +0200

    feat: check only arrays are used with the function

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const WrongValueType = require("./WrongValueType");
 
 const flatten = (arrayToFlatten) => {
+    if (!Array.isArray(arrayToFlatten)) {
+        throw new WrongValueType(arrayToFlatten)
+    }
 }
 
 module.exports = flatten;
